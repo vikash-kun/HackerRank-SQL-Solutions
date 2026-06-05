8548ca2731b1acf6a4f67229db52b3ac6eaa3047
vikash-kun <12345vikash@gmail.com>
2026-06-05 09:51:17 +0530

    adding Q9-Q13

--- solutions.sql
+++ solutions.sql
@@ -96,3 +96,23 @@ SELECT CITY, LENGTH(CITY) FROM (
   FROM STATION
 ) WHERE RN = 1;
 
+
+
+/* Q11 -Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.*/
+
+
+/* solution*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY LIKE 'A%' 
+   OR CITY LIKE 'E%' 
+   OR CITY LIKE 'I%' 
+   OR CITY LIKE 'O%' 
+   OR CITY LIKE 'U%'
+   OR CITY LIKE 'a%' 
+   OR CITY LIKE 'e%' 
+   OR CITY LIKE 'i%' 
+   OR CITY LIKE 'o%' 
+   OR CITY LIKE 'u%';
+
