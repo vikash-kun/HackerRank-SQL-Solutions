06c44fa2c97bf51814730479f542d510bcd7e58b
vikash-kun <12345vikash@gmail.com>
2026-06-05 09:56:18 +0530

    adding Q9-Q13

--- solutions.sql
+++ solutions.sql
@@ -137,10 +137,14 @@ WHERE CITY LIKE '%a'
 
 
 
-/* Q13-*/
+/* Q13-Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.*/
 
 
 /*solution*/
 
 
--
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY,1)) IN ('a','e','i','o','u')
+  AND LOWER(RIGHT(CITY,1)) IN ('a','e','i','o','u');
+
