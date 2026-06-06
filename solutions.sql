4209f0a3ae5c581a9657045107881eb330f2a3c0
vikash-kun <12345vikash@gmail.com>
2026-06-06 10:15:40 +0530

    added Q16

--- solutions.sql
+++ solutions.sql
@@ -173,3 +173,17 @@ WHERE LOWER(LEFT(CITY,1)) not in ('a','e','i','o','u');
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LOWER(RIGHT(CITY,1)) not in ('a','e','i','o','u');
+
+
+
+
+/*Q16-Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.*/
+
+
+/*solution*/
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY,1)) not IN  ('a','e','i','o','u')
+  or LOWER(RIGHT(CITY,1))  not IN ('a','e','i','o','u');
